2a915ce99ce6192418613ee52cda25f0ed33dcf0
Arpan Srivastava <108794407+AAArpan@users.noreply.github.com>
2024-10-20 13:23:49 +0530

    Add files via upload

---
+++ my_py_pkg/setup.py
@@ -0,0 +1,41 @@
+from setuptools import find_packages, setup
+
+package_name = 'my_py_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='arpan',
+    maintainer_email='arpan@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            "py_node = my_py_pkg.my_first_node:main",
+            "robot_news_station = my_py_pkg.robot_news_station:main",
+            "smartphone = my_py_pkg.smartphone:main",
+            "number_publisher = my_py_pkg.number_publisher:main",
+            "number_counter = my_py_pkg.number_counter:main",
+            "add_two_ints_server = my_py_pkg.add_two_ints_server:main",
+            "add_two_ints_client = my_py_pkg.add_two_ints_client:main",
+            "hw_status_publisher = my_py_pkg.hw_status_publisher:main",
+            "led_panel = my_py_pkg.led_panel:main",
+            "battery = my_py_pkg.battery:main",
+            "robot_commmand_publisher = my_py_pkg.robot_command:main",
+            "camera_stream = my_py_pkg.camera_stream:main",
+            "send_waypoints = my_py_pkg.send_waypoints:main"
+            
+            
+
+        ],
+    },
+)
